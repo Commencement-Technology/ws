f0400bd4ca4dfb129d2b0de24dec39420c9ec62e
Ed Halliwell <edhalliwell@hotmail.com>
2024-03-25 14:46:42 +0000

    message styles

--- packages/client/src/message.tsx
+++ packages/client/src/message.tsx
@@ -1,16 +1,30 @@
+import { styled } from 'styled-components';
 import { Message as MessageType } from './message-list';
+
+const StyledTextMe = styled.p`
+  background-color: lightcoral;
+  border-radius: 1em;
+  padding: 0.5em 1em;
+  margin: 0.5em;
+  margin-right: 4em;
+`;
+
+const StyledTextThem = styled.p`
+  background-color: lightblue;
+  border-radius: 1em;
+  text-align: right;
+  padding: 0.5em 1em;
+  margin: 0.5em;
+  margin-left: 4em;
+`;
 
 export const Message = ({ message, myUserId }: { message: MessageType; myUserId: string }) => {
   return (
     <>
       {message.userId === myUserId ? (
-        <li key={message.id} style={{ color: 'red', padding: '0.5em 1em' }}>
-          Me: {message.content}
-        </li>
+        <StyledTextMe key={message.id}>Me: {message.content}</StyledTextMe>
       ) : (
-        <li key={message.id} style={{ color: 'blue', textAlign: 'right', padding: '0.5em 1em' }}>
-          Them: {message.content}
-        </li>
+        <StyledTextThem key={message.id}>Them: {message.content}</StyledTextThem>
       )}
     </>
   );
